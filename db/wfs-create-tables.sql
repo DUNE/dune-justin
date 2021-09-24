72a012581c50a83bdf9ef23bc2545e488e338254
Andrew McNab <andrew.mcnab@cern.ch>
2021-09-24 12:40:27 +0100

    Import DB testing SQL scripts

---
+++ db/wfs-create-tables.sql
@@ -0,0 +1,142 @@
+# mysqldump --no-data wfd > wfd-create-tables.sql 
+
+-- MySQL dump 10.14  Distrib 5.5.68-MariaDB, for Linux (x86_64)
+--
+-- Host: localhost    Database: wfd
+-- ------------------------------------------------------
+-- Server version	5.5.68-MariaDB
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `bootstraps`
+--
+
+DROP TABLE IF EXISTS `bootstraps`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `bootstraps` (
+  `request_id` mediumint(8) unsigned NOT NULL,
+  `stage_id` tinyint(3) unsigned NOT NULL,
+  `bootstrap` text NOT NULL,
+  UNIQUE KEY `request_id` (`request_id`,`stage_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `files`
+--
+
+DROP TABLE IF EXISTS `files`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `files` (
+  `file_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `request_id` mediumint(8) unsigned NOT NULL,
+  `stage_id` tinyint(3) unsigned NOT NULL,
+  `did` varchar(255) NOT NULL,
+  `state` enum('unallocated','allocated','processed') NOT NULL DEFAULT 'unallocated',
+  PRIMARY KEY (`file_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `replicas`
+--
+
+DROP TABLE IF EXISTS `replicas`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `replicas` (
+  `rse_id` smallint(5) unsigned NOT NULL,
+  `file_id` int(10) unsigned NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `requests`
+--
+
+DROP TABLE IF EXISTS `requests`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `requests` (
+  `request_id` mediumint(8) unsigned NOT NULL AUTO_INCREMENT,
+  `state` enum('draft','submitted','approved','running','paused','checking','completed','deleted') NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `created` datetime NOT NULL,
+  `submitted` datetime DEFAULT NULL,
+  `approved` datetime DEFAULT NULL,
+  `checking` datetime DEFAULT NULL,
+  `completed` datetime DEFAULT NULL,
+  PRIMARY KEY (`request_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `sites_storages`
+--
+
+DROP TABLE IF EXISTS `sites_storages`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `sites_storages` (
+  `site_name` varchar(255) NOT NULL,
+  `rse_id` smallint(5) unsigned NOT NULL,
+  `location` enum('site','nearby','accessible') NOT NULL DEFAULT 'site',
+  UNIQUE KEY `rse_id` (`rse_id`,`site_name`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `stages`
+--
+
+DROP TABLE IF EXISTS `stages`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `stages` (
+  `request_id` mediumint(8) unsigned NOT NULL,
+  `stage_id` tinyint(3) unsigned NOT NULL,
+  `min_inputs` tinyint(3) unsigned NOT NULL,
+  `max_inputs` tinyint(3) unsigned NOT NULL,
+  `max_wall_seconds` mediumint(8) unsigned DEFAULT NULL,
+  `max_rss_bytes` bigint(20) unsigned DEFAULT NULL,
+  `any_location` tinyint(1) NOT NULL DEFAULT '0',
+  UNIQUE KEY `request_id` (`request_id`,`stage_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `storages`
+--
+
+DROP TABLE IF EXISTS `storages`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `storages` (
+  `rse_name` varchar(255) NOT NULL,
+  `rse_id` smallint(5) unsigned NOT NULL AUTO_INCREMENT,
+  PRIMARY KEY (`rse_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2021-08-18 12:04:10
---
+++ db/wfs-test-values.sql
@@ -0,0 +1,66 @@
+USE wfd
+
+DELETE FROM requests;
+DELETE FROM stages;
+DELETE FROM bootstraps;
+DELETE FROM files;
+DELETE FROM replicas;
+DELETE FROM storages;
+DELETE FROM sites_storages;
+
+ALTER TABLE requests AUTO_INCREMENT=1;
+ALTER TABLE files AUTO_INCREMENT=1;
+ALTER TABLE storages AUTO_INCREMENT=1;
+
+# Set up three sites: Manchester and RAL-Tier1 nearby each other, FNAL across the Atlantic
+
+INSERT INTO storages SET rse_name='FNAL_DCACHE';
+INSERT INTO storages SET rse_name='MANCHESTER';
+INSERT INTO storages SET rse_name='RAL_ECHO';
+
+INSERT INTO sites_storages SET rse_id=1,site_name='US_FNAL',location='site';
+INSERT INTO sites_storages SET rse_id=1,site_name='UK_RAL-Tier1',location='accessible';
+INSERT INTO sites_storages SET rse_id=1,site_name='UK_Manchester',location='accessible';
+
+INSERT INTO sites_storages SET rse_id=2,site_name='UK_RAL-Tier1',location='nearby';
+INSERT INTO sites_storages SET rse_id=2,site_name='UK_Manchester',location='site';
+
+INSERT INTO sites_storages SET rse_id=3,site_name='UK_RAL-Tier1',location='site';
+INSERT INTO sites_storages SET rse_id=3,site_name='UK_Manchester',location='nearby';
+
+# Create one request where files can only be processed from nearby sites
+INSERT INTO requests SET state='running',name='test request 1',created=NOW(),submitted=NOW(),approved=NOW();
+INSERT INTO stages SET request_id=1,stage_id=1,min_inputs=1,max_inputs=1,max_wall_seconds=86400,max_rss_bytes=8123123,any_location=FALSE;
+INSERT INTO bootstraps SET request_id=1,stage_id=1,bootstrap='echo hello 1,1';
+
+INSERT INTO files SET request_id=1,stage_id=1,did='file.01.01';
+INSERT INTO files SET request_id=1,stage_id=1,did='file.01.02';
+INSERT INTO files SET request_id=1,stage_id=1,did='file.01.03';
+
+INSERT INTO replicas SET file_id=1,rse_id=1;
+INSERT INTO replicas SET file_id=1,rse_id=2;
+INSERT INTO replicas SET file_id=1,rse_id=3;
+
+INSERT INTO replicas SET file_id=2,rse_id=2;
+
+INSERT INTO replicas SET file_id=3,rse_id=2;
+INSERT INTO replicas SET file_id=3,rse_id=3;
+
+# Create another request where files can be processed from any site
+INSERT INTO requests SET state='running',name='test request 2',created=NOW(),submitted=NOW(),approved=NOW();
+INSERT INTO stages SET request_id=2,stage_id=1,min_inputs=1,max_inputs=1,max_wall_seconds=86400,max_rss_bytes=8123123,any_location=TRUE;
+INSERT INTO bootstraps SET request_id=2,stage_id=1,bootstrap='echo hello 2,1';
+
+INSERT INTO files SET request_id=2,stage_id=1,did='file.02.01';
+INSERT INTO files SET request_id=2,stage_id=1,did='file.02.02';
+INSERT INTO files SET request_id=2,stage_id=1,did='file.02.03';
+
+INSERT INTO replicas SET file_id=4,rse_id=1;
+INSERT INTO replicas SET file_id=4,rse_id=2;
+INSERT INTO replicas SET file_id=4,rse_id=3;
+
+INSERT INTO replicas SET file_id=5,rse_id=2;
+
+INSERT INTO replicas SET file_id=6,rse_id=2;
+INSERT INTO replicas SET file_id=6,rse_id=3;
+
