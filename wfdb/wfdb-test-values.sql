622f7c26eae86beeb5677e1986b0113e88f62604
Andrew McNab <andrew.mcnab@cern.ch>
2021-10-05 12:57:27 +0100

    Tidy up and metadata fixes

--- wfdb/wfdb-test-values.sql
+++ wfdb/wfdb-test-values.sql
@@ -36,7 +36,9 @@ INSERT INTO sites_storages SET rse_id=3,site_name='UK_Manchester',location='near
 # Create one request where files can only be processed from nearby sites
 INSERT INTO requests SET state='running',name='test request 1',created=NOW(),submitted=NOW(),approved=NOW();
 INSERT INTO stages SET request_id=1,stage_id=1,max_inputs=1,max_wall_seconds=86400,max_rss_bytes=8123123,min_processors=1,max_processors=1,any_location=FALSE;
-INSERT INTO bootstraps SET request_id=1,stage_id=1,bootstrap='##wfa_files_json##\n##wfa_files_did_rse##';
+INSERT INTO bootstraps SET request_id=1,stage_id=1;
+
+##wfa_files_did_rse##';
 
 INSERT INTO files SET request_id=1,stage_id=1,file_did='file.01.01';
 INSERT INTO files SET request_id=1,stage_id=1,file_did='file.01.02';
@@ -55,7 +57,7 @@ INSERT INTO replicas SET file_id=3,rse_id=1;
 # Create another request where files can be processed from any site
 INSERT INTO requests SET state='running',name='test request 2',created=NOW(),submitted=NOW(),approved=NOW();
 INSERT INTO stages SET request_id=2,stage_id=1,max_inputs=1,max_wall_seconds=86400,max_rss_bytes=8123123,min_processors=1,max_processors=1,any_location=TRUE;
-INSERT INTO bootstraps SET request_id=2,stage_id=1,bootstrap='##wfa_files_json##\n##wfa_files_did_rse##';
+INSERT INTO bootstraps SET request_id=2,stage_id=1;
 
 INSERT INTO files SET request_id=2,stage_id=1,file_did='file.02.01';
 INSERT INTO files SET request_id=2,stage_id=1,file_did='file.02.02';
@@ -71,3 +73,22 @@ INSERT INTO replicas SET file_id=5,rse_id=3;
 
 INSERT INTO replicas SET file_id=6,rse_id=1;
 
+# Use same script for all bootstraps
+
+UPDATE bootstraps SET bootstrap="
+cat <<EOF >files.txt
+##wfa_files_did##
+EOF
+
+for i in `cat files.txt`
+do
+ echo Could process $i
+done
+
+echo 'Files JSON: ##wfa_files_json##'
+
+echo 'Request = ##wfa_request_id## and Stage = ##wfa_stage_id##'
+
+wfa_cookie='##wfa_cookie##'
+echo The WFA Cookie is $wfa_cookie
+";
