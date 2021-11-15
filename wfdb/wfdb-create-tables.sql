1715cdf9b62696291d992af7820bf7b1e7aaea3c
Andrew McNab <andrew.mcnab@cern.ch>
2021-11-15 14:13:36 +0000

    Use table of users

--- wfdb/wfdb-create-tables.sql
+++ wfdb/wfdb-create-tables.sql
@@ -80,6 +80,7 @@ CREATE TABLE `requests` (
   `approved` datetime DEFAULT NULL,
   `checking` datetime DEFAULT NULL,
   `completed` datetime DEFAULT NULL,
+  `submitter_id` smallint(5) unsigned NOT NULL DEFAULT '0',
   PRIMARY KEY (`request_id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -148,6 +149,21 @@ CREATE TABLE `storages` (
   PRIMARY KEY (`rse_id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `users` (
+  `user_id` smallint(5) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `x509dn` varchar(255) NOT NULL,
+  PRIMARY KEY (`user_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -158,4 +174,4 @@ CREATE TABLE `storages` (
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2021-11-01 14:22:59
+-- Dump completed on 2021-11-15 13:41:40
