7194b3461b674f587587a43e06bb0b85cdd453d1
Andrew McNab <andrew.mcnab@cern.ch>
2022-04-02 20:54:14 +0100

    Update dashboard and command

--- testing/hello-world-bootstrap.sh
+++ testing/hello-world-bootstrap.sh
@@ -3,6 +3,8 @@
 # Get an unprocessed file from this stage
 did_pfn_rse=`$WFS_PATH/wfs-get-file`
 did=`echo $did_pfn_rse | cut -f1 -d' '`
+pfn=`echo $did_pfn_rse | cut -f2 -d' '`
+rse=`echo $did_pfn_rse | cut -f3 -d' '`
 
 # We say we processed whatever we were given
 echo "$did" > wfs-processed-inputs.txt
@@ -11,6 +13,6 @@ echo "$did" > wfs-processed-inputs.txt
 echo > wfs-unprocessed-inputs.txt
 
 # Hello world
-echo "Hello world"
+echo "Hello world $pfn"
 
 exit 0
