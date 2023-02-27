3974cb81b336be9ad7e4d07bc6fdca643cbbb78a
Raja Nandakumar <rajanandakumar@gmail.com>
2023-02-27 06:20:35 +0000

    Update entrypoint to use justin-* commands

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # dune-wfs
 DUNE Workflow System
+
+Notes : Working on dune-wfs-test.cern.ch (accessible from lxplus)
+
+cd /home/wfs/dune-justin
+git pull (if needed)
+docker-compose -f justin-infra.yaml build
+docker-compose -f justin-agents.yaml build
--- justin/justinEntrypoint.sh
+++ justin/justinEntrypoint.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 set -e
 
-./justin/agents/wfs-finder
-./justin/agents/wfs-stage-cache
-./justin/agents/wfs-info-collector
-./justin/agents/wfs-job-factory
+./justin/agents/justin-finder
+./justin/agents/justin-stage-cache
+./justin/agents/justin-info-collector
+./justin/agents/justin-job-factory
 
 tail -f /var/log/wfs/*
