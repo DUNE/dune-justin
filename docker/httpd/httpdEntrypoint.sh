c90a8833d017113f1626918ab72f1ae824d7d1e3
Raja Nandakumar <rajanandakumar@gmail.com>
2023-03-03 03:58:11 +0000

    Get certbot running - root and nonroot

--- docker/httpd/httpdEntrypoint.sh
+++ docker/httpd/httpdEntrypoint.sh
@@ -4,6 +4,7 @@ set -e
 # ls -l /etc/grid-security/	
 certbot --apache -d dune-wfs-test.cern.ch
 
-httpd -D FOREGROUND
+
+sudo su nonroot httpd -D FOREGROUND
 
 # certbot --apache -d wfs-pro.dune.hep.ac.uk -d wfs.dune.hep.ac.uk
