2d299d329479507e0bbcc613668d685f36d8224a
Raja Nandakumar <rajanandakumar@gmail.com>
2023-03-06 08:34:59 +0000

    Debug to why container does not start

--- docker/httpd/httpdEntrypoint.sh
+++ docker/httpd/httpdEntrypoint.sh
@@ -4,7 +4,12 @@ set -e
 # ls -l /etc/grid-security/	
 # certbot --apache -d dune-wfs-test.cern.ch
 
+which httpd
 
-httpd -D FOREGROUND
+pwd
+
+ls -l /usr/local/apache2/
+
+/usr/local/apache2/httpd -D FOREGROUND
 
 # certbot --apache -d wfs-pro.dune.hep.ac.uk -d wfs.dune.hep.ac.uk
