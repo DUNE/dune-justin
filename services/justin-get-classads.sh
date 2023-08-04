428bd3888043cb04d2c821ab8aae133000532db0
Andrew McNab <andrew.mcnab@cern.ch>
2023-08-04 11:38:40 +0100

    Add apptainer test to justin-get-classads.sh

--- services/justin-get-classads.sh
+++ services/justin-get-classads.sh
@@ -1,10 +1,28 @@
 #!/bin/bash
-#
-# justin-get-classads.sh - script to be run before HTCondor jobs
-#                          to get just-in-time classads from justIN
-#
-# This script was modelled on blocklist.sh from gco_scripts-master
-#
+:<<'EOF'
+
+justin-get-classads.sh - script to be run before HTCondor jobs
+                         to get just-in-time classads from justIN
+
+This script was modelled on blocklist.sh from gco_scripts-master
+
+Test with
+
+cat <<'EOFEOF' >/tmp/justin-get-classads.glidein_config.txt
+function add_config_line { echo add_config_line $* 
+}
+function add_condor_vars_line { echo add_condor_vars_line $* 
+}
+:<<'EEOOFF'
+ADD_CONFIG_LINE_SOURCE /tmp/justin-get-classads.glidein_config.txt
+CONDOR_VARS_FILE /tmp/justin-get-classads.glidein_config.txt
+ERROR_GEN_PATH /tmp/justin-get-classads.glidein_config.txt
+EEOOFF
+EOFEOF
+
+./justin-get-classads.sh /tmp/justin-get-classads.glidein_config.txt
+
+EOF
 
 glidein_config="$1"
 
@@ -43,9 +61,26 @@ source ${add_config_line_source}
 
 #----------MAIN SCRIPT----------#
 
+export HAS_INNER_APPTAINER=False
+if [ -x /cvmfs/oasis.opensciencegrid.org/mis/apptainer/current/bin/apptainer ]
+then
+  /cvmfs/oasis.opensciencegrid.org/mis/apptainer/current/bin/apptainer \
+  shell --shell /usr/bin/hostname \
+  /cvmfs/singularity.opensciencegrid.org/fermilab/fnal-wn-sl7:osg3.6  
+  if [ $? = 0 ] ; then
+    export HAS_INNER_APPTAINER=True
+  fi
+fi
+
+( echo '==== justin-get-classads glidein_config ===='
+  cat "${glidein_config}" 
+  echo '==== justin-get-classads printenv ===='
+  printenv ) > justin-get-classads-posted.txt
+
 for prodev in pro dev
 do
-  http_code=`curl --silent --insecure --data-binary "@${glidein_config}" \
+  http_code=`curl --silent --insecure \
+    --data-binary @justin-get-classads-posted.txt \
     --output justin-get-classads-$prodev.txt \
     --write-out "%{http_code}\n" \
     --connect-timeout 10 \
@@ -54,13 +89,12 @@ do
     http://justin-allocator-$prodev.dune.hep.ac.uk/api/get-classads \
     2>/dev/null`
 
-  if [ "$http_code" = 200 -a -r justin-get-classads-$prodev.txt ]
-  then
+  if [ "$http_code" = 200 -a -r justin-get-classads-$prodev.txt ] ; then
     cat justin-get-classads-$prodev.txt | (
       while read line
       do
         advertise $line "S"
-      done 
+      done
     )
   fi
 done
