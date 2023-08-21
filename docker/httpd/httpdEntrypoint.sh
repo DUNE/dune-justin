94442e287b048d53dda54f93f786be509d68eb8e
Raja Nandakumar Raja.Nandakumar@stfc.ac.uk <nraja@justin-test-frontend01.dune.hep.ac.uk>
2023-08-21 15:14:36 +0000

    Command to figure out where mod_wsgi is located

--- docker/httpd/httpdEntrypoint.sh
+++ docker/httpd/httpdEntrypoint.sh
@@ -5,11 +5,12 @@ set -e
 # certbot --apache -d dune-wfs-test.cern.ch
 
 which httpd
-
+httpd -M
+mod_wsgi-express module-config
 pwd
 
 whoami
-
+echo "Starting apache ..."
 httpd -D FOREGROUND
 
 # certbot --apache -d wfs-pro.dune.hep.ac.uk -d wfs.dune.hep.ac.uk
