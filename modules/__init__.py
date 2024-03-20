29c3080b785e2877168f1db35bc1b273c36121d9
Andrew McNab <andrew.mcnab@cern.ch>
2024-03-20 10:28:55 +0000

    backport reloading of X.509 proxies

--- modules/__init__.py
+++ modules/__init__.py
@@ -631,3 +631,21 @@ def fixPfn(pfn):
     pfn = pfn.replace(old, new)
     
   return pfn
+
+def checkProxyStrings():
+  global jobsNoRolesProxyString, jobsProductionProxyString
+  
+  try:
+    with open(jobsNoRolesProxyFile, 'rb') as f:
+      jobsNoRolesProxyString = f.read()
+  except:
+    print('Failed loading X.509 proxy from %s : %s'
+          % (jobsNoRolesProxyFile, str(e)), file=sys.stderr)
+
+  try:
+    with open(jobsProductionProxyFile, 'rb') as f:
+      jobsProductionProxyString = f.read()
+  except:
+    print('Failed loading X.509 proxy from %s : %s'
+          % (jobsProductionProxyFile, str(e)), file=sys.stderr)
+ 
--- modules/justin_version.py
+++ modules/justin_version.py
@@ -1,2 +1,2 @@
 # This file must by valid Python AND valid Bash!
-justinVersion='00.14.00'
+justinVersion='00.14.01'
