c56237eed55665089f7ae3da01f146a3bf2add0d
Andrew McNab <andrew.mcnab@cern.ch>
2025-03-06 16:12:44 +0000

    Start of finishing state

--- database/justindb-create-tables.sql
+++ database/justindb-create-tables.sql
@@ -200,7 +200,8 @@ CREATE TABLE IF NOT EXISTS `workflows` (
   `workflow_id` mediumint(8) unsigned NOT NULL AUTO_INCREMENT,
   `campaign_id` mediumint(8) unsigned NOT NULL DEFAULT 0,
   `state` enum('draft','submitted','approved','running',
-     'paused','checking','finished','deleted') NOT NULL DEFAULT 'finished',
+     'paused','checking','finished','deleted', 'finishing') 
+     NOT NULL DEFAULT 'finished',
   `state_message` text NOT NULL DEFAULT '',
   `scope_id` smallint(5) unsigned NOT NULL DEFAULT 0,
   `condor_group_id` smallint(5) unsigned NOT NULL DEFAULT 0,
