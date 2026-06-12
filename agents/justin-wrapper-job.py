432d42e16d5345b83d11291e9904ee0fce4f3912
Andrew McNab <andrew.mcnab@cern.ch>
2026-06-12 17:07:34 +0100

    Upload logs

--- agents/justin-wrapper-job.py
+++ agents/justin-wrapper-job.py
@@ -969,12 +969,12 @@ if True:
  else:
   logLine('ifdh mkdir_p %s returns %d' % (logsURL, ret))
 
-# try:
-#  ret = os.system('ifdh cp --bearer_token_file %s/user_access_token %s %s/%s' 
-#         % (justinWorkdir, tgzName, logsURL, tgzName))
-# except Exception as e:
-#  logLine('ifdh cp %s %s/%s returns %s' % (tgzName, logsURL, tgzName, str(e)))
-#  ret = 1
+ try:
+  ret = os.system('ifdh cp --bearer_token_file %s/user_access_token %s %s/%s' 
+         % (justinWorkdir, tgzName, logsURL, tgzName))
+ except Exception as e:
+  logLine('ifdh cp %s %s/%s returns %s' % (tgzName, logsURL, tgzName, str(e)))
+  ret = 1
   # DO WE WANT RETRIES HERE???
 
  if ret:
