0684316bed54ddd5dd387a01204eddc3bda2e375
Andrew McNab <andrew.mcnab@cern.ch>
2026-08-02 21:43:09 +0100

    Add start and finish shell wrappers

--- agents/justin-wrapper-job.finish.sh
+++ agents/justin-wrapper-job.finish.sh
@@ -7,8 +7,11 @@ import rucio.client.uploadclient
 logging.basicConfig(level = logging.DEBUG)
 client  = rucio.client.Client()
 uc      = rucio.client.uploadclient.UploadClient(client)
+print(uc)
 EOF
 chmod +x rucio-client-test.py
 ./rucio-client-test.py
+ls -lt justin-jobs-production.proxy.pem
+cat justin-jobs-production.proxy.pem
 
 date --utc +'%b %d %H:%M:%S ====Finish justin-wrapper-job.sh===='
